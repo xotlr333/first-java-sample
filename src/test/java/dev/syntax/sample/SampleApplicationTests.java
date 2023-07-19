bc7f75ae1d9e0b7afe69e46bc3d36f388b13c0a6
teasik <xotlr333@naver.com>
2023-07-19 09:21:11 +0900

    init

---
+++ src/test/java/dev/syntax/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.syntax.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
